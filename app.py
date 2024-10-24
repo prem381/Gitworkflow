ce680c8800e34993cc88c0c4f55de27e660bda02
prem381 <126052245+prem381@users.noreply.github.com>
2024-10-24 16:02:58 +0530

    created basic code files

---
+++ app.py
@@ -0,0 +1,7 @@
+# app.py
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print("The sum of 5 and 3 is:", add(5, 3))
+
---
+++ test-app.py
@@ -0,0 +1,14 @@
+# test_app.py
+import unittest
+from app import add
+
+class TestApp(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(5, 3), 8)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(0, 0), 0)
+        self.assertEqual(add(-5, -5), -10)
+
+if __name__ == "__main__":
+    unittest.main()
+
